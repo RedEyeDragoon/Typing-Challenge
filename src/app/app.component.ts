532e466c5276d88b3bd053e3084777edf5ac3414
RedEyeDragoon <ximaginebreaksx@gmail.com>
2020-07-14 05:34:18 -0400

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, VERSION } from '@angular/core';
+import { lorem } from 'faker';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  input = '';
+  enteredText = '';
+
+  randomText = lorem.sentence();
+
+  onInput(value: string) {
+    this.enteredText = value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
+}
